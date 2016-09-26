5555c9bddc2af08c21c64e5596e8b33abff171c4
Anna Dolan <anna.dolan@gmail.com>
2016-09-26 12:21:51 -0600

    final commit

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,27 +3,26 @@ require 'sinatra/reloader'
 
 NUMBER = rand(100)
 
-
 def check_guess(guess)
   if guess == 0 && NUMBER != 0
+    @color = "#ffffff"
     "Guess a number!"
   elsif guess > NUMBER && guess <= (NUMBER + 5)
-    @color = "pink"
+    @color = "#ffe6e6"
     "Too high!"
   elsif guess > (NUMBER + 5)
-    @color = "red"
+    @color = "#ff1a1a"
     "Way too high!"
   elsif guess < NUMBER && guess >= (NUMBER - 5)
-    @color = "pink"
+    @color = "#ffe6e6"
     "Too low!"
   elsif guess  < (NUMBER - 5)
-    @color = "red"
+    @color = "#ff1a1a"
     "Way too low!"
   elsif guess == NUMBER
-    @color = "green"
+    @color = "#00ff00"
     "You got it right! The SECRET NUMBER is #{NUMBER}"
   end
-
 end
 
 get '/' do
