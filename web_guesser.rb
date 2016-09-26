a854958c1e9a0252946d45493d5507c3b7da4ce4
Anna Dolan <anna.dolan@gmail.com>
2016-09-26 13:10:54 -0600

    fixed a thing

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,33 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-NUMBER = rand(100)
 
-def check_guess(guess)
-  if guess == 0 && NUMBER != 0
-    @color = "#ffffff"
-    "Guess a number!"
-  elsif guess > NUMBER && guess <= (NUMBER + 5)
-    @color = "#ffe6e6"
-    "Too high!"
-  elsif guess > (NUMBER + 5)
-    @color = "#ff1a1a"
-    "Way too high!"
-  elsif guess < NUMBER && guess >= (NUMBER - 5)
-    @color = "#ffe6e6"
-    "Too low!"
-  elsif guess  < (NUMBER - 5)
-    @color = "#ff1a1a"
-    "Way too low!"
-  elsif guess == NUMBER
-    @color = "#00ff00"
-    "You got it right! The SECRET NUMBER is #{NUMBER}"
-  end
-end
+NUMBER = rand(100)
 
 get '/' do
 guess = params['guess'].to_i
-message = check_guess(guess)
-erb :index, :locals => {:number => NUMBER, :message => message, :color => @color}
+color, message = check_guess(guess)
+erb :index, :locals => {:number => NUMBER, :message => message, :color => color}
+end
 
+def check_guess(guess)
+  if guess == 0 && NUMBER != 0
+    ["#ffffff", "Guess a number!"]
+  elsif guess > NUMBER && guess <= (NUMBER + 5)
+    ["#ffe6e6", "Too high!"]
+  elsif guess > (NUMBER + 5)
+    ["#ff1a1a", "Way too high!"]
+  elsif guess < NUMBER && guess >= (NUMBER - 5)
+    ["#ffe6e6", "Too low!"]
+  elsif guess  < (NUMBER - 5)
+    ["#ff1a1a", "Way too low!"]
+  elsif guess == NUMBER
+    ["#00ff00", "You got it right! The SECRET NUMBER is #{NUMBER}"]
+  end
 end
